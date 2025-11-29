f155605b69d7c291897ae2358d8148aaaed664f2
jchuayta <carloshuayta112@gmail.com>
2025-11-28 22:26:39 -0400

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { readFileSync, writeFileSync } from 'fs'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 const replaceGAId = () => {
   return {
     name: 'replace-ga-id',
     buildStart() {
-      const gaId = process.env.VITE_GA_MEASUREMENT_ID || ''
-      const indexPath = resolve(__dirname, 'index.html')
+      const gaId = process.env.VITE_GA_MEASUREMENT_ID || 'G-QLMQ1ZJCFY'
+      const __dirname = fileURLToPath(new URL('.', import.meta.url))
+      const indexPath = `${__dirname}index.html`
       let html = readFileSync(indexPath, 'utf-8')
       
       html = html.replace(
